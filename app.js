ad8f0e7a74536d7131a1f92745c5154f7cf4d91a
Wayne <urrightguy@gmail.com>
2021-06-02 21:36:49 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const Todo = require('./models/todo') // 載入 Todo model
 
 // setting mongoose
 const mongoose = require('mongoose') // 載入 mongoose
@@ -22,7 +23,10 @@ app.set('view engine', 'handlebars')
 
 // setting routes
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos: todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 // starts the express server and listening for connections.
