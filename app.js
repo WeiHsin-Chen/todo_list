d4d7744844e367b2bf65d08526663c5b06aabe39
Wayne <urrightguy@gmail.com>
2021-06-07 23:49:28 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
 const Todo = require('./models/todo') // 載入 Todo model
+const methodOverride = require('method-override')  // 載入 method-override
+const routes = require('./routes')    // 引用路由器
+
 
 // setting mongoose
 const mongoose = require('mongoose') // 載入 mongoose
@@ -24,71 +27,63 @@ app.set('view engine', 'handlebars')
 // setting body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// 載入 method-override
-const methodOverride = require('method-override')
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 
-// setting routes
-app.get('/', (req, res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .sort({ _id: 'asc' }) // 根據 _id 升冪(ascending)排序；降冪 (desscending) 
-    .then(todos => res.render('index', { todos: todos })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
-})
+// 將 request 導入路由器
+app.use(routes)
 
-// create todo data
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
+// // create todo data
+// app.get('/todos/new', (req, res) => {
+//   return res.render('new')
+// })
 
-app.post('/todos', (req, res) => {
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
-})
+// app.post('/todos', (req, res) => {
+//   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+//   return Todo.create({ name })     // 存入資料庫
+//     .then(() => res.redirect('/')) // 新增完成後導回首頁
+//     .catch(error => console.log(error))
+// })
 
-// display todo details
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
+// // display todo details
+// app.get('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then((todo) => res.render('detail', { todo }))
+//     .catch(error => console.log(error))
+// })
 
-// revise todo data
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
+// // revise todo data
+// app.get('/todos/:id/edit', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then((todo) => res.render('edit', { todo }))
+//     .catch(error => console.log(error))
+// })
 
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
+// app.put('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   const { name, isDone } = req.body
+//   return Todo.findById(id)
+//     .then(todo => {
+//       todo.name = name
+//       todo.isDone = isDone === 'on'
+//       return todo.save()
+//     })
+//     .then(() => res.redirect(`/todos/${id}`))
+//     .catch(error => console.log(error))
+// })
 
-// delete todo data
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// // delete todo data
+// app.delete('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .then(todo => todo.remove())
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 
 // starts the express server and listening for connections.
 app.listen(port, () => {
