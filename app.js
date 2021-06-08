6c422c46466205451230e96c2560ea81c230f01a
Wayne <urrightguy@gmail.com>
2021-06-08 22:51:50 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -8,17 +8,8 @@ const Todo = require('./models/todo') // 載入 Todo model
 const methodOverride = require('method-override')  // 載入 method-override
 const routes = require('./routes')    // 引用路由器
 
-
-// setting mongoose
-const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-const db = mongoose.connection // 取得資料庫連線狀態
-db.on('error', () => {                // 連線異常
-  console.log('mongodb error!')
-})
-db.once('open', () => {               // 連線成功
-  console.log('mongodb connected!')
-})
+// connect mongoose with ./config/mongoose
+require('./config/mongoose')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -33,57 +24,6 @@ app.use(methodOverride('_method'))
 // 將 request 導入路由器
 app.use(routes)
 
-// // create todo data
-// app.get('/todos/new', (req, res) => {
-//   return res.render('new')
-// })
-
-// app.post('/todos', (req, res) => {
-//   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-//   return Todo.create({ name })     // 存入資料庫
-//     .then(() => res.redirect('/')) // 新增完成後導回首頁
-//     .catch(error => console.log(error))
-// })
-
-// // display todo details
-// app.get('/todos/:id', (req, res) => {
-//   const id = req.params.id
-//   return Todo.findById(id)
-//     .lean()
-//     .then((todo) => res.render('detail', { todo }))
-//     .catch(error => console.log(error))
-// })
-
-// // revise todo data
-// app.get('/todos/:id/edit', (req, res) => {
-//   const id = req.params.id
-//   return Todo.findById(id)
-//     .lean()
-//     .then((todo) => res.render('edit', { todo }))
-//     .catch(error => console.log(error))
-// })
-
-// app.put('/todos/:id', (req, res) => {
-//   const id = req.params.id
-//   const { name, isDone } = req.body
-//   return Todo.findById(id)
-//     .then(todo => {
-//       todo.name = name
-//       todo.isDone = isDone === 'on'
-//       return todo.save()
-//     })
-//     .then(() => res.redirect(`/todos/${id}`))
-//     .catch(error => console.log(error))
-// })
-
-// // delete todo data
-// app.delete('/todos/:id', (req, res) => {
-//   const id = req.params.id
-//   return Todo.findById(id)
-//     .then(todo => todo.remove())
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
 
 // starts the express server and listening for connections.
 app.listen(port, () => {
