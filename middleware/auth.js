2a5596836f0c8996fd55bcf4470105c935bcfaed
Wayne <urrightguy@gmail.com>
2021-08-07 23:37:11 +0800

    feat: delete console.log

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,9 @@
 module.exports = {
   authenticator: (req, res, next) => {
-    console.log(req.isAuthenticated())
     // 學期三 U25解釋
     if (req.isAuthenticated()) {
-      console.log('success')
       return next()
     }
-    console.log('failure')
     res.redirect('/users/login')
   }
 }
