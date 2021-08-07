ffb0f7de3d0daaa2149c00d33992746eff6e2292
Wayne <urrightguy@gmail.com>
2021-08-08 00:50:36 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const Todo = require('./models/todo') // 載入 Todo model
 const methodOverride = require('method-override')  // 載入 method-override
+const flash = require('connect-flash')
 const routes = require('./routes')    // 引用路由器
 const usePassport = require('./config/passport')  // 載入設定檔，要寫在 express-session 以後
 
@@ -34,9 +35,14 @@ app.use(session({
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
+// 導入 flash
+app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 
